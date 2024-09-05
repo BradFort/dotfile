575bcffbfdd3384d97b0513643ace99726ff95aa
bradley <bradleycfortin@gmail.com>
2024-09-05 12:32:08 -0400

    updateInstall1

---
+++ Install.sh
@@ -0,0 +1,11 @@
+mkdir bin
+mkdir cegep
+mkdir projets
+
+sudo pacman -Sy git
+sudo pacman -Sy vim
+sudo pacman -Sy gcc
+sudo pacman -Sy gdb
+sudo pacman -Sy python3
+sudo pacman -Sy python3-pip
+sudo pacman -Sy python3-venv
